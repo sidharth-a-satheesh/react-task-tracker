8a147b80e9b37d79ca704f781d4309b7c63f2df0
Sidharth <sidharth.a.satheesh@gmail.com>
2022-06-23 21:36:21 +0530

    added tasks

--- src/App.js
+++ src/App.js
@@ -1,12 +1,34 @@
+import { useState } from "react"
 import Header from "./components/Header";
 import Tasks from "./components/Tasks";
 
 function App({title}) {
-
+  const [tasks, setTasks] = useState(
+    [
+        {
+            id: 1,
+            text: 'Doctors Appoinment',
+            day: 'Feb 15th at 2:30pm',
+            remainder: true,
+        },
+        {
+            id: 2,
+            text: 'Meeting at School',
+            day: 'June 15th at 1:30pm',
+            remainder: true,
+        },
+        {
+            id: 3,
+            text: 'Food Shopping',
+            day: 'March 15th at 2:30pm',
+            remainder: false,
+        },
+    ]
+  )
   return (
     <div className="container">
       <Header />
-      <Tasks />
+      <Tasks tasks={tasks}/>
     </div>
   );
 }
